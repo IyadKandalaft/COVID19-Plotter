4819220813a394f13177cfc210f11d04b795c28f
Iyad Kandalaft <ik@iyadk.com>
2020-03-21 18:09:24 -0400

    Added subtractive and cumulative data calculation options

--- coviddata.py
+++ coviddata.py
@@ -12,12 +12,11 @@ class COVIDEnum(Enum):
     RECOVERED = 'recovered'
 
 
-class DataFile(NamedTuple):
-    data_type: COVIDEnum
-    path: str
-
-
 class COVIDDataParser(object):
+    class DataFile(NamedTuple):
+        data_type: COVIDEnum
+        path: str
+
     def __init__(self, infected_csv, dead_csv, recovered_csv, skip_first=2):
         super().__init__()
         self.data_files = []
@@ -97,18 +96,24 @@ class COVIDData(dict):
         }
 
     def _add_date(self, country: str, date: str):
-        self.data[country][COVIDEnum.INFECTED][date] = None
-        self.data[country][COVIDEnum.RECOVERED][date] = None
-        self.data[country][COVIDEnum.DEAD][date] = None
+        self.data[country][COVIDEnum.INFECTED][date] = 0
+        self.data[country][COVIDEnum.RECOVERED][date] = 0
+        self.data[country][COVIDEnum.DEAD][date] = 0
 
     def _add_data_bydate(self, data_type: COVIDEnum, country: str, date: str, value: int):
         if country not in self.data:
             self._add_country(country)
-        self.data[country]['total'][data_type] += value
 
         if date not in self.data[country][data_type]:
             self._add_date(country, date)
-        self.data[country][data_type][date] = value
+        
+        # Add data from new record to existing value for that date
+        # Existing value could be from records separted by province
+        self.data[country][data_type][date] += value
+
+        # Set the total to the latest record we have for that counrty
+        latest_date = sorted(list(self.data[country][data_type])).pop()
+        self.data[country]['total'][data_type] = self.data[country][data_type][latest_date]
 
     def get_total_infected(self, country: str):
         return self.get_total(COVIDEnum.INFECTED, country)
@@ -122,36 +127,57 @@ class COVIDData(dict):
     def get_total(self, data_type: COVIDEnum, country: str):
         return self.data[country]['total'][data_type]
 
-    def get_infected_bydate(self, country: str, date: date):
-        return self.get_bydate(COVIDEnum.INFECTED, country, date)
+    def get_infected_bydate(self, country: str, date: date, cumulative: bool = True):
+        return self.get_bydate(COVIDEnum.INFECTED, country, date, cumulative)
 
-    def get_dead_bydate(self, country: str, date: date):
-        return self.get_bydate(COVIDEnum.DEAD, country, date)
+    def get_dead_bydate(self, country: str, date: date, cumulative: bool = True):
+        return self.get_bydate(COVIDEnum.DEAD, country, date, cumulative)
 
-    def get_recovered_bydate(self, country: str, date: date):
-        return self.get_bydate(COVIDEnum.RECOVERED, country, date)
+    def get_recovered_bydate(self, country: str, date: date, cumulative: bool = True):
+        return self.get_bydate(COVIDEnum.RECOVERED, country, date, cumulative)
 
-    def get_bydate(self, data_type: COVIDEnum, country: str, date: date):
+    def get_bydate(self, data_type: COVIDEnum, country: str, date: date, cumulative: bool = True):
         str_date = COVIDData._format_date(date)
-        if str_date in self.data[country][data_type]:
+        if str_date not in self.data[country][data_type]:
+            return None
+        
+        if cumulative:
             return self.data[country][data_type][str_date]
-        return None
+        
+        dates = sorted(list(self.data[country][data_type].keys()))
+        # Subtractive data is calculated by subtracting from the target date's
+        # cumulative value the previous date's cumulative value
+        prev_day = dates(dates.index(date) - 1)
+        return self.data[country][data_type][str_date] - self.data[country][data_type][prev_day]
 
-    def get_infected_bycountry(self, country):
-        return self.get_bycountry(COVIDEnum.INFECTED, country)
+    def get_infected_bycountry(self, country, cumulative: bool = True):
+        return self.get_bycountry(COVIDEnum.INFECTED, country, cumulative)
 
-    def get_dead_bycountry(self, country):
-        return self.get_bycountry(COVIDEnum.DEAD, country)
+    def get_dead_bycountry(self, country, cumulative: bool = True):
+        return self.get_bycountry(COVIDEnum.DEAD, country, cumulative)
 
-    def get_recovered_bycountry(self, country):
-        return self.get_bycountry(COVIDEnum.RECOVERED, country)
+    def get_recovered_bycountry(self, country, cumulative: bool = True):
+        return self.get_bycountry(COVIDEnum.RECOVERED, country, cumulative)
 
-    def get_bycountry(self, data_type:COVIDEnum, country: str):
-        if country in self.data:
+    def get_bycountry(self, data_type:COVIDEnum, country: str, cumulative: bool = True):
+        if country not in self.data:
+            return None
+
+        if cumulative:
             return self.data[country][data_type]
-        return None
+        
+        # Sort the keys (dates) because we can't rely on their order
+        dates = sorted(list(self.data[country][data_type].keys()))
+        # Subtractive data must be calculated from the existing cumulative data
+        sub_data = self.data[country][data_type].copy()
+        prev_day_total = 0
+        for day in dates:
+            prev_value = sub_data[day]
+            sub_data[day] -= prev_day_total
+            prev_day_total = prev_value
+            
+        return sub_data
 
     @staticmethod
     def _format_date(date: date):
-        str_date_list = list(map(str, [date.year, date.month, date.day]))
-        return '-'.join(str_date_list)
+        return str(date.year) + '-' + str(date.month).zfill(2) + '-' + str(date.day).zfill(2)
